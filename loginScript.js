08fecc7cb701b61661e3babc3198b7e446b98512
karn07 <karnrajan07@gmail.com>
2021-05-15 21:03:55 +0530

    added admin login

--- loginScript.js
+++ loginScript.js
@@ -1,13 +1,28 @@
 const submitBtn = document.querySelector("#submit");
 const passF = document.querySelector("#pwd");
-const nameF = document.querySelector("#name");
+const nameF = document.querySelector("#Name");
+
+
+submitBtn.addEventListener('click', () => {
+    const pass = passF.value
+    const username = nameF.value
+    if(!(username && pass)) {
+        alert(`Username or Password Missing!!!`)
+    }
+    else {
+        if(!(pass[0] == pass[0].toUpperCase())){
+            alert(`Password must start with Uppercase letter!!!`)
+        }
+    }
+
+})
 
 submitBtn.addEventListener('click', () => {
     const pass = passF.value
     const username = nameF.value
 
     if(!(username && pass)) {
-        alert(`Name or Password Missing!!!`)
+        alert(`Username or Password Missing!!!`)
     }
     else {
         if(!(pass[0] == pass[0].toUpperCase())){
